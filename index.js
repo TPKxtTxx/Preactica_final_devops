25a992f4528516baecec657fd2bddb39106e452c
TPKxtTxx <132860744+TPKxtTxx@users.noreply.github.com>
2023-12-15 16:46:28 -0400

    Agregar archivos iniciales

---
+++ index.js
@@ -0,0 +1,5 @@
+function obtenerMensaje(){
+  return 'Hola, Mundo Otto Rafael Gonzalez (2022-0121)';
+}
+
+module.exports = obtenerMensaje;
---
+++ index.test.js
@@ -0,0 +1,5 @@
+const holaMund = require('./index');
+
+test('Comprobacion del mensage de saludo', () => { 
+    expect(holaMund()).toBe('Hola, Mundo Otto Rafael Gonzalez (2022-0121)');
+}); 
